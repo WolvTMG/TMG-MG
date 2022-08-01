67e86bc8c2ddef88c43af6b8e3e77ea685b743d2
Wolv <unconfigured@null.spigotmc.org>
2022-08-01 15:24:03 +0100

    added a start menu

--- main.py
+++ main.py
@@ -13,15 +13,22 @@ def again():
     global score
     global list
 
+    print("\n" * 120)
+    print("Wrong\n")
+
     answer = input("Would you like to play again?\n\nAnswer: ").lower()
     if answer == 'yes' or answer == 'yea' or answer == 'y' or answer == 'yeah' or answer == 'ye':
         score = 0
         list = []
         main()
-    else:
+    elif answer == 'no' or answer == 'nah' or answer == 'naah' or answer == 'nein' or answer == 'nien' or answer == 'nahh':
         print("Thanks for playing!")
         time.sleep(2)
         sys.exit()
+    else:
+        print("\n" * 120)
+        e = input("Please input a valid argument")
+        again()
 
 def main():
     global score
@@ -38,15 +45,42 @@ def main():
         score = score + 1
         print("\n" * 120)
         main()
+    elif answer == 'new' and word in list:
+        again()
     elif answer == 'seen' and word in list:
         print("Correct")
         list.append(word)
         score = score + 1
         print("\n" * 120)
         main()
-    else:
-        print("Wrong\n")
-        time.sleep(2)
+    elif answer == 'seen' and word not in list:
         again()
+    else:
+        print("Please input a valid argument\n")
+        time.sleep(2)
+        main()
 
-main()
+def start():
+    print("\n" * 120)
+    print("""
+ ________  ________  __  ________
+/_  __/  |/  / ___/ /  |/  / ___/
+ / / / /|_/ / (_ / / /|_/ / (_ / 
+/_/ /_/  /_/\___/ /_/  /_/\___/  
+""")
+    ready = input("\n\n\n\nStart\nCredits\n\nChoice: ").lower()
+
+    if ready == 'start':
+        main()
+    elif ready == 'credits':
+        print("\n" * 120)
+        e = input("All coded by WolvTMG#0945\n\nPress [Enter] to return")
+        print("\n" * 120)
+        start()
+    else:
+        print("\n" * 120)
+        e = input("Please input a valid argument")
+        print("\n" * 120)
+        start()
+
+start()
